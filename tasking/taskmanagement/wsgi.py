4b1c5cb605c0821748af5e73b68930a11f77835c
Jason Moore <jason@digitalreach.com.au>
2020-04-21 20:59:02 +0800

    Docker Static File Fix

--- tasking/taskmanagement/wsgi.py
+++ tasking/taskmanagement/wsgi.py
@@ -4,14 +4,14 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 """
 import confy
 from django.core.wsgi import get_wsgi_application
+from dj_static import Cling, MediaCling
 import os
-print ("BASE 2")
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-print (BASE_DIR)
 confy.read_environment_file(BASE_DIR+"/.env")
 os.environ.setdefault("BASE_DIR", BASE_DIR)
 
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "taskmanagement.settings")
-application = get_wsgi_application()
+#application = get_wsgi_application()
+application = Cling(MediaCling(get_wsgi_application()))
